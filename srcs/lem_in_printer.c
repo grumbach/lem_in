26332dd768c3eee3fd215a18bf4396bd28ba3317
Anselme GRUMBACH <agrumbac@e3r12p12.42.fr>
2017-04-09 21:11:24 +0200

    --amend

--- srcs/lem_in_printer.c
+++ srcs/lem_in_printer.c
@@ -6,54 +6,54 @@
 /*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/09 18:44:28 by agrumbac          #+#    #+#             */
-/*   Updated: 2017/04/09 18:59:39 by agrumbac         ###   ########.fr       */
+/*   Updated: 2017/04/09 19:23:49 by agrumbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
 void                   lem_print_answ(const t_xy pathlen, void *pathlists, \
-                                       void *rooms, const t_lemsize *size)
-{
-       t_rooms         (*room)[size->rooms + 1];
-       int                     (*pathlist)[size->maxflux][size->rooms + 1];
-       int                     ants;
-       int                     i;
+	void *rooms, const t_lemsize *size)
+	{
+		t_rooms         (*room)[size->rooms + 1];
+		int                     (*pathlist)[size->maxflux][size->rooms + 1];
+		int                     ants;
+		int                     i;
 
-       pathlist = pathlists;
-       room = rooms;
-       ants = size->ants;
-       ft_printf("%s\n", size->par);
-
-
-
-
-       //shortest path
-       for (int i = 0; i <= pathlen.x; i++)
-               ft_printf("%s[%d]->", (*room)[(*pathlist)[0][i]].roomname, \
-                       (*pathlist)[0][i]);
-       ft_printf("\n\n");
+		pathlist = pathlists;
+		room = rooms;
+		ants = size->ants;
+		ft_printf("%s\n", size->par);
 
 
 
-       int             path;
-       int             used_paths = 1;
 
-       while (--ants + 1)
-       {
-               path = -1;
-               while (++path < used_paths)
-               {
-                       i = -1;
-                       while ((*pathlist)[path][++i] != -1)
-                       {
-                               ft_printf("=%d=%d=", i, (*pathlist)[path][i]);
-                               ft_printf("L%d-%s", size->ants - ants, (*room)[(*pathlist)[path][i]].roomname);
-                               ft_printf("\n");
-                       }
-               }
-       }
-}
+		//shortest path
+		for (int i = 0; i <= pathlen.x; i++)
+		ft_printf("%s[%d]->", (*room)[(*pathlist)[0][i]].roomname, \
+		(*pathlist)[0][i]);
+		ft_printf("\n\n");
+
+
+
+		int             path;
+		int             used_paths = 1;
+
+		while (--ants + 1)
+		{
+			path = -1;
+			while (++path < used_paths)
+			{
+				i = -1;
+				while ((*pathlist)[path][++i] != -1)
+				{
+					//    ft_printf("=%d=%d=", i, (*pathlist)[path][i]);
+					ft_printf("L%d-%s", size->ants - ants, (*room)[(*pathlist)[path][i]].roomname);
+					ft_printf("\n");
+				}
+			}
+		}
+	}
 
 
 
