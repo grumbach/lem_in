d1145633a98f14eeb81f3012bc779fa89fa9bcae
Anselme GRUMBACH <agrumbac@e2r11p14.42.fr>
2017-05-12 13:54:43 +0200

    all normed all is GOOD FOR FACTORY

--- lem_in.h
+++ lem_in.h
@@ -6,7 +6,7 @@
 /*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/09 09:31:15 by agrumbac          #+#    #+#             */
-/*   Updated: 2017/05/12 10:45:22 by agrumbac         ###   ########.fr       */
+/*   Updated: 2017/05/12 13:47:54 by agrumbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define LEM_IN_H
 
 # include "libft.h"
+# include "ft_printf.h"
 # include <errno.h>
 # include <sys/types.h>
 # include <sys/uio.h>
--- srcs/lem_in_printer.c
+++ srcs/lem_in_printer.c
@@ -6,7 +6,7 @@
 /*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/09 18:44:28 by agrumbac          #+#    #+#             */
-/*   Updated: 2017/05/12 12:20:58 by agrumbac         ###   ########.fr       */
+/*   Updated: 2017/05/12 13:54:16 by agrumbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,31 @@
 static void		print_ants_moves(const void *pathlens, \
 				const void *pathlists, void *rooms, const t_lemsize *size)
 {
-	t_rooms					(*room)[size->rooms + 1];
-	const int				(*pathlist)[size->maxflux][size->rooms + 1];
-	const int				(*pathlen)[size->maxflux];
-	t_xy					explore;
-	int						ant_nb;
-	const int				end = lem_end(rooms, size);
+	t_rooms		(*room)[size->rooms + 1];
+	const int	(*pathlist)[size->maxflux][size->rooms + 1];
+	const int	(*pathlen)[size->maxflux];
+	t_xy		explore;
+	int			ant_nb;
+	const int	end = lem_end(rooms, size);
+	const char	*color[6] = {RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN};
 
 	pathlist = pathlists;
 	room = rooms;
 	pathlen = pathlens;
 	while ((ant_nb = (*room)[end].ants) != size->ants && (explore.y = -1))//while not all ants in end
-		while (++explore.y < size->maxflux && ++ant_nb && \
+		while (++explore.y < size->maxflux &&
 		(explore.x = (*pathlen)[explore.y] ? (*pathlen)[explore.y] + 1 : 0) && \
-			ft_printf("\n"))//all path from 1st
+		ft_printf("\n"))//all path from 1st
 			while (explore.x--)//all rooms from last (closest to end)
-			{
-				// ft_printf("y%d, x%d : %s\n", explore.y, explore.x, 0);//
 				if ((*pathlist)[explore.y][explore.x] != end && \
 				(*pathlist)[explore.y][explore.x] != -1 && \
-				(*room)[	(*pathlist)[explore.y][explore.x]	].ants)//if in a path && there is ant here
+				(*room)[(*pathlist)[explore.y][explore.x]].ants && ++ant_nb)//if in a path && there is ant here
 				{
-					(*room)[	(*pathlist)[explore.y][explore.x]	].ants--;
-					(*room)[	(*pathlist)[explore.y][explore.x + 1]	].ants++;
-					ft_printf("L%d-%s ", ant_nb, (*room)[(*pathlist)[explore.y][explore.x + 1]].roomname);
-					ant_nb++;
+					(*room)[(*pathlist)[explore.y][explore.x]].ants--;
+					(*room)[(*pathlist)[explore.y][explore.x + 1]].ants++;
+					ft_printf("L%s%d-%s ""\e[0m", color[ant_nb % 6], ant_nb, \
+					(*room)[(*pathlist)[explore.y][explore.x + 1]].roomname);
 				}//move ants towards end
-			}
 }
 
 void			lem_print_answ(const void *pathlens, \
